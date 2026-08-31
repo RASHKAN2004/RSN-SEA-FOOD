80a523a7c1dff530d387ccdd38b4e83d3b25793f
RASHKAN2004 <mmohamedraskhan@gmail.com>
2026-08-31 20:44:38 +0530

    error fixed code

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,10 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 async function connectDB() {
   try {
     const uri = process.env.MONGO_URI;
     if (!uri) {
-      throw new Error('MONGO_URI is not set in environment variables');
+      throw new Error("MONGO_URI is not set in environment variables");
     }
     const conn = await mongoose.connect(uri);
     console.log(`[MongoDB] Connected: ${conn.connection.host}`);
